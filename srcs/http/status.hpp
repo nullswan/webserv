820d91221401bc3c1fce553ac5cfd663c8246dea
c3b5aw <dev@c3b5aw.dev>
2021-12-03 14:05:36 +0100

    update http code list

--- srcs/http/status.hpp
+++ srcs/http/status.hpp
@@ -7,14 +7,46 @@
 namespace Webserv {
 namespace Http {
 
-static std::map<int, std::string> status_code = {
+static std::map<int, std::string> http_code_list = {
+	{400, "Bad Request"},
+	{401, "Unauthorized"},
+	{402, "Payment Required"},
 	{403, "Forbidden"},
-	{404, "Not found"},
-	{500, "Server error"},
+	{404, "Not Found"},
+	{405, "Method Not Allowed"},
+	{406, "Not Acceptable"},
+	{407, "Proxy Authentication Required"},
+	{408, "Request Timeout"},
+	{409, "Conflict"},
+	{410, "Gone"},
+	{411, "Length Required"},
+	{412, "Precondition Failed"},
+	{413, "Payload Too Large"},
+	{414, "URI Too Long"},
+	{415, "Unsupported Media Type"},
+	{416, "Range Not Satisfiable"},
+	{417, "Expectation Failed"},
+	{422, "Unprocessable Entity"},
+	{426, "Upgrade Required"},
+	{428, "Precondition Required"},
+	{429, "Too Many Requests"},
+	{431, "Request Header Fields Too Large"},
+	{451, "Unavailable For Legal Reasons"},
+	{500, "Internal Server Error"},
+	{501, "Not Implemented"},
+	{502, "Bad Gateway"},
+	{503, "Service Unavailable"},
+	{504, "Gateway Timeout"},
+	{505, "HTTP Version Not Supported"},
+	{506, "Variant Also Negotiates"},
+	{507, "Insufficient Storage"},
+	{508, "Loop Detected"},
+	{510, "Not Extended"},
+	{511, "Network Authentication Required"},
 };
 
 const std::string &resolve_code(const int &status_code) const {
-	std::map<int, std::string>::iterator it = status_code.find(status_code);
+	std::map<int, std::string>::iterator it = http_code_list.find(status_code);
 	if (!it)
 		return "Unknown error";
 	return it->second;
