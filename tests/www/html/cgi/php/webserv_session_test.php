05081d98f9e08f78bfb162bd0403a1ec64dc4fee
c3b5aw <dev@c3b5aw.dev>
2021-12-10 18:40:11 +0100

    scripts update

--- tests/www/html/cgi/php/webserv_session_test.php
+++ tests/www/html/cgi/php/webserv_session_test.php
@@ -4,6 +4,10 @@ setcookie("WEBSERV_SESSION_TEST", "1");
 setcookie("WEBSERV_SESSION_TEST_2", "2");
 setcookie("WEBSERV_SESSION_TEST_3", "3");
 
-echo $_COOKIE["WEBSERV_SID"];
+if (isset($_COOKIE["WEBSERV_SESSION_ID"])) {
+	echo "Session id: " . $_COOKIE["WEBSERV_SESSION_ID"] . "<br>";
+} else {
+	echo "No session id<br>";
+}
 
 ?>
