711da62dbedb8f26613c3ccc0b39241fd09fe439
c3b5aw <dev@c3b5aw.dev>
2021-12-13 16:07:49 +0100

    fix unexpected behavior in error_page parsing

--- srcs/conf/parser.hpp
+++ srcs/conf/parser.hpp
@@ -233,7 +233,7 @@ class Parser {
 		_skip_whitespaces(bucket);
 
 		char	last_chr = (*bucket)[bucket->size() - 1];
-		if (last_chr != ';' && (inside_location_block && last_chr != '{')) {
+		if (last_chr != ';' && !(inside_location_block && last_chr == '{')) {
 			invalid_delimiter_error(*bucket);
 			throw std::runtime_error("invalid delimiter");
 		}
@@ -305,17 +305,19 @@ class Parser {
 				}
 				case CONF_BLOCK_ERROR_PAGE: {
 					_extract_value("error_page", &line, false);
-					if (line.size() < 6
-						|| !_is_digits(line.substr(0, 3))
+
+					if (line.size() < 6 || !_is_digits(line.substr(0, 3))
 						|| line.find(" ") == std::string::npos
 						|| line.substr(0, line.find(" ")).size() != 3)
-						return invalid_value_error(line, line_nbr);
+							return invalid_value_error(line, line_nbr);
+
 					int error_code = atoi(line.substr(0, line.find(" ")).c_str());
 					std::string page_path = line.substr(line.find(" ") + 1, line.size());
 					std::string source;
 					if (!_dump_file(page_path, &source))
 						return invalid_value_error(page_path, line_nbr);
 					current_block->set_error_page(error_code, source);
+					break;
 				}
 				case CONF_BLOCK_INDEX: {
 					_extract_value("index", &line, false);
@@ -330,7 +332,7 @@ class Parser {
 						if (it->size() > 0)
 							current_block->add_index(*it);
 					}
-					continue;
+					break;
 				}
 				case CONF_SERVER_LOCATION: {
 					if (scope == 2)
@@ -346,7 +348,7 @@ class Parser {
 					if (!block)
 						return invalid_value_error(line, line_nbr);
 					current_block = block;
-					continue;
+					break;
 				}
 				case CONF_SERVER_LISTEN: {
 					_extract_value("listen", &line, false);
